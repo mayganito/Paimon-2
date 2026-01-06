57f3b12c4ddaeb7f552df5c09b53b22dffe84f6a
May <rachithakaushan9@gmail.com>
2026-01-06 14:17:40 +0530

    Update

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "", //- The host name or IP of the lavalink server.
+			host: "lavalink", //- The host name or IP of the lavalink server.
 			port: 80, // The port that lavalink is listening to. This must be a number!
 			password: "", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
